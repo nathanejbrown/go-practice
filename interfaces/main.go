d1a0c0383297521881ab7d165000cda1de326dd2
Nathan Brown <nathanejbrown@gmail.com>
2023-09-25 11:17:14 -0600

    set up bot interace

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,10 +1,24 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (eb englishBot) getGreeting() string {
